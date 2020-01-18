1649c86d103ad03dc5f1fe8e59b21e34db3994d0
Kyle Andrews <kylea@page.works>
2020-01-18 12:57:12 -0500

    Added inital Craft collection script and base translations object constructor

--- cli.js
+++ cli.js
@@ -2,10 +2,9 @@
 
 const fs = require("fs");
 const path = require("path");
-const glob = require("glob");
 const semver = require("semver");
-const ora = require("ora");
 const yargs = require("yargs").argv;
+const glob = require("glob");
 
 const cwd = process.cwd();
 
@@ -42,3 +41,118 @@ if (!config) {
     console.log("Missing config file. Learn more at https://github.com/codewithkyle/dragomanjs#configuration");
     process.exit(1);
 }
+
+/** Validate required config values */
+if (typeof config.project === "undefined") {
+    console.log("Invalid config. The project value is missing.");
+    process.exit(1);
+}
+if (typeof config.content === "undefined") {
+    console.log("Invalid config. The content value is missing.");
+    process.exit(1);
+} else {
+    if (!Array.isArray(config.content) && typeof config.content === "string") {
+        config.content = [config.content];
+    } else {
+        console.log("Invalid config. The content value must be a string or an array of strings.");
+        process.exit(1);
+    }
+}
+if (typeof config.locals === "undefined") {
+    console.log("Invalid config. The locals array is missing.");
+    process.exit(1);
+} else {
+    if (!Array.isArray(config.locals)) {
+        console.log("Invalid config. The locals value must be an array of strings");
+        process.exit(1);
+    }
+}
+
+class Dragoman {
+    constructor() {
+        this.run();
+        this.translations = {};
+    }
+
+    parseValues(file, handle) {
+        return new Promise((resolve, reject) => {
+            fs.readFile(file, (error, buffer) => {
+                if (error) {
+                    reject(error);
+                }
+                const data = buffer
+                    .toString()
+                    .replace(/\<\?php\s+/g, "")
+                    .replace(/return\s+\[/g, "")
+                    .replace(/\s+\]\;/g, "")
+                    .trim();
+                const pairs = data.split(/(,\n)|(,\s+\n)|([,])$/g);
+                for (let i = 0; i < pairs.length; i++) {
+                    const pair = pairs[i].split(/\=\>/);
+                    const key = pair[0]
+                        .trim()
+                        .replace(/^[\"\']|[\"\']$/g, "")
+                        .trim();
+                    const value = pair[1]
+                        .trim()
+                        .replace(/^[\"\']|[\"\']$/g, "")
+                        .trim();
+                    this.translations[handle][key] = value;
+                }
+                resolve();
+            });
+        });
+    }
+
+    constructInitialObject() {
+        return new Promise((resolve, reject) => {
+            const tempDir = path.join(__dirname, "temp");
+            if (!fs.existsSync(tempDir)) {
+                reject("Temporary directory wasn't generated.");
+            }
+            glob(`${tempDir}/*.php`, (error, files) => {
+                if (error) {
+                    reject(error);
+                } else if (!files.length) {
+                    resolve();
+                }
+                let parsed = 0;
+                for (let i = 0; i < files.length; i++) {
+                    const localHandle = files[i].replace(/.*[\\\/]/, "").replace(/\.php/, "");
+                    this.translations[localHandle] = {};
+                    this.parseValues(files[i], localHandle)
+                        .then(() => {
+                            parsed++;
+                            if (parsed === files.length) {
+                                resolve();
+                            }
+                        })
+                        .catch(error => {
+                            reject(error);
+                        });
+                }
+            });
+        });
+    }
+
+    async run() {
+        try {
+            let collector;
+            switch (config.project) {
+                case "craft":
+                    const Tool = require(path.join(__dirname, "craft.js"));
+                    collector = new Tool(config);
+                    break;
+                default:
+                    throw `Invalid config project value: ${config.project}. Learn more at https://github.com/codewithkyle/dragomanjs#configuration`;
+            }
+            const collectedKeys = await collector.run();
+            await this.constructInitialObject();
+        } catch (error) {
+            console.log(error);
+            console.log("\n");
+            process.exit(1);
+        }
+    }
+}
+new Dragoman();
---
+++ craft.js
@@ -0,0 +1,160 @@
+const glob = require("glob");
+const fs = require("fs");
+const path = require("path");
+
+const cwd = process.cwd();
+
+/** Matches anything between double curly brackets */
+const aggressiveMatcher = new RegExp(/(\{\{|\{\{\s).*?(\}\}|\s\}\})/, "g");
+
+/** Matches anything between single or double quotes that end with a |t */
+const passiveMatcher = new RegExp(/(["'].*?["']\|t)/, "g");
+
+export class Craft {
+    constructor(config) {
+        this.config = config;
+        this.tempDir = null;
+    }
+
+    getTemplates() {
+        return new Promise(resolve => {
+            let files = [];
+            for (let i = 0; i < this.config.content.length; i++) {
+                const basePath = path.resolve(cwd, this.config.content[i]);
+                const newFiles = glob.sync(`${basePath}/**/*.twig`);
+                files = [...files, ...newFiles];
+            }
+            resolve(files);
+        });
+    }
+
+    collectRawStrings(templates) {
+        return new Promise((resolve, reject) => {
+            if (!templates.length) {
+                reject("No templates provided.");
+            }
+            let strings = [];
+            let parsed = 0;
+            for (let i = 0; i < templates.length; i++) {
+                fs.readFile(templates[i], (error, buffer) => {
+                    if (error) {
+                        reject(error);
+                    }
+                    const newStrings = buffer.toString().match(aggressiveMatcher);
+                    if (newStrings) {
+                        for (let k = 0; k < newStrings.length; k++) {
+                            strings.push(newStrings[k]);
+                        }
+                    }
+                    parsed++;
+                    if (parsed === templates.length) {
+                        resolve(strings);
+                    }
+                });
+            }
+        });
+    }
+
+    cleanStrings(rawStrings) {
+        return new Promise(resolve => {
+            if (!rawStrings.length) {
+                resolve(rawStrings);
+            }
+            const cleanedStrings = [];
+            for (let i = 0; i < rawStrings.length; i++) {
+                const translationString = rawStrings[i].match(passiveMatcher);
+                if (translationString) {
+                    const cleanString = translationString[0].replace(/(\|t)/g, "").replace(/^["']|["']$/gm, "");
+                    cleanedStrings.push(cleanString);
+                }
+            }
+            resolve(cleanedStrings);
+        });
+    }
+
+    prugeDuplicates(strings) {
+        return new Promise(resolve => {
+            if (!strings.length) {
+                resolve(strings);
+            }
+            const uniqueStrings = [];
+            for (let i = 0; i < strings.length; i++) {
+                let isUnique = true;
+                const currString = strings[i];
+                for (let k = 0; k <= uniqueStrings.length; k++) {
+                    if (currString === uniqueStrings[k]) {
+                        isUnique = false;
+                        break;
+                    }
+                }
+                if (isUnique) {
+                    uniqueStrings.push(strings[i]);
+                }
+            }
+            resolve(uniqueStrings);
+        });
+    }
+
+    createTempDirectory() {
+        return new Promise((resolve, reject) => {
+            const tempPath = path.join(__dirname, "temp");
+            if (fs.existsSync(tempPath)) {
+                fs.rmdirSync(tempPath, { recursive: true });
+            }
+            fs.mkdir(tempPath, error => {
+                if (error) {
+                    reject(error);
+                }
+                this.tempDir = tempPath;
+                resolve();
+            });
+        });
+    }
+
+    copyExistingFiles() {
+        return new Promise((resolve, reject) => {
+            const basePath = path.join(cwd, "translations");
+            glob(`${basePath}/**/*.php`, (error, files) => {
+                if (error) {
+                    reject(error);
+                } else if (!files.length) {
+                    resolve();
+                }
+                let copied = 0;
+                for (let i = 0; i < files.length; i++) {
+                    const pathValues = files[i].replace(/.*(translations)[\\\/]/g, "").split(/[\\\/]/);
+                    fs.copyFile(files[i], `${this.tempDir}/${pathValues[0]}.php`, error => {
+                        if (error) {
+                            reject(error);
+                        }
+                        copied++;
+                        if (copied === files.length) {
+                            resolve();
+                        }
+                    });
+                }
+            });
+        });
+    }
+
+    async run() {
+        try {
+            /** Get template files */
+            const templateFiles = await this.getTemplates();
+
+            /** Get strings */
+            const rawStrings = await this.collectRawStrings(templateFiles);
+            const cleanStrings = await this.cleanStrings(rawStrings);
+            const uniqueStrings = await this.prugeDuplicates(cleanStrings);
+
+            /** Handle existing translation files */
+            await this.createTempDirectory();
+            await this.copyExistingFiles();
+
+            /** Pass the unique keys back to the CLI script */
+            return uniqueStrings;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
